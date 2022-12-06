53395fd3402cdae53414dc1d5a26e793d47cc4f5
Niklas Diekhoener <niklasdiekhoener@gmail.com>
2022-12-06 07:45:46 +0100

    d6

--- d6p1.py
+++ d6p1.py
@@ -1,12 +1,20 @@
 from aocd.models import Puzzle
 
-ex_in = """"""
+ex_in = """bvwbjplbgvbhsrlpgdmjqwftvncz"""
 
 puzzle = Puzzle(day=6, year=2022)
 or_in = puzzle.input_data
-#or_in = ex_in
+# or_in = ex_in
 
-
+for i in range(len(or_in) - 3):
+    if or_in[i] != or_in[i + 1] and \
+            or_in[i] != or_in[i + 2] and \
+            or_in[i] != or_in[i + 3] and \
+            or_in[i + 1] != or_in[i + 2] and \
+            or_in[i + 1] != or_in[i + 3] and \
+            or_in[i + 2] != or_in[i + 3]:
+        res = i + 4
+        break
 
 print(res)
 puzzle.answer_a = res
--- d6p2.py
+++ d6p2.py
@@ -1,12 +1,22 @@
 from aocd.models import Puzzle
 
-ex_in = """"""
+ex_in = """bvwbjplbgvbhsrlpgdmjqwftvncz"""
 
 puzzle = Puzzle(day=6, year=2022)
 or_in = puzzle.input_data
-#or_in = ex_in
+# or_in = ex_in
+
+def f():
+    for i in range(len(or_in) - 13):
+        sub_in = or_in[i:i + 14]
+        for m in range(14):
+            if sub_in.count(sub_in[m]) != 1:
+                break
+            else:
+                if m == 13:
+                    return i + 14
 
 
-
+res = f()
 print(res)
 puzzle.answer_b = res
